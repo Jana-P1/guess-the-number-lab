45240105e81520958115a36fbe9d7c78185be544
Jana Pascual <jacpascual@gmail.com>
2022-01-30 21:13:50 -0500

    Render method completed, called render() and getGuess in game.play()

--- app.js
+++ app.js
@@ -10,15 +10,17 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
       // Log every player guess (correct/incorrect) into the prevGuesses arr using push method
-      for(let i = 0; i < game.prevGuesses.length; i++){
-        previousGuess.push(this.getGuess)
-      }
+      console.log(game.getGuess());
+      while(this.getGuess !== this.secretNum){
+        this.preGuesses.push(this.getGuess)
+      };
+      console.log(this.render())
   },
   prevGuesses: [],
   getGuess: function() {
     // Creates a prompt instructing the player to guess a number within the given range
     // The player input is assigned to a variable that will be used later in the game
-    let input = prompt((`Enter a guess between ${smallestNum} and ${biggestNum}.`));
+    let input = window.prompt(`Enter a guess between ${smallestNum} and ${biggestNum}.`);
     console.log(input); 
     let playerInput = parseInt(input)
     while (playerInput = true) {
@@ -34,14 +36,35 @@ const game = {
   return playerInput
   },
   render: function(){
-    let totalGuesses = this.prevGuesses.length 
-    if(this.getGuess === this.secretNum) {
-      console.log(`Congrats! You guessed the number in ${this.totalGuesses}`)
-    } else if(this.getGuess > ) {}
-
-    }
+    // render method compares the player's guess with the secretNum
     
-  },
+    let totalGuesses = this.prevGuesses.length
+    // totalGuesses finds the length of the preGuesses array
+    
+    let listOfGuesses = this.prevGuesses.join() 
+    // listOfGuesses changes the list of prevGuesses into a string
+    
+    if(this.getGuess === this.secretNum) {
+      // If the player guesses correctly, the player is alerted that they won and displays the total number of attempts => totalGuesses variable
+      
+      console.log(`Congrats! You guessed the number in ${this.totalGuesses}`)
+    
+
+      //If the player's guess is too low/too high, the player is alerted and all previous guesses are displayed 
+    } else if(this.getGuess > this.secretNum) {
+      console.log(`Your guess is too high. Previous guesses: ${listOfGuesses}`)
+    } else if(this.getGuess < this.secretNum) {
+      console.log(`Your guess is too low. Previous guesses: ${listOfGuesses}`)
+    }
+      
+    }, 
+    } 
+
+    console.log(game.play())
+    
+
+
+  
 
 
 
